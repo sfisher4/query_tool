209815fd68cc425b7ff7a18812bb49650cfaf648
sfisher <shannah.elizabeth@gmail.com>
2017-12-05 15:30:45 -0700

    initial

--- server.R
+++ server.R
@@ -1,89 +1,94 @@
-library(shiny)
-library(ggplot2)
-library(dplyr)
-library(reshape2)
-library(tidyr)
-library(magrittr)
-library(stringr)
-library(ggpubr)
 
 #ASK:
 # Currently my rank is integer based and doesn't have '.5' if there is >1 with that particular # of isolates. Still correctly ranks them though
 
-# helpers
-perc_sim <- function(text, min_sim, min_iso) {
-  #CONSTRAINTS: The Pattern must start on the 2nd column and go to the 41st column.
-  cgfdb <- cgfdb_testset2 %>%
-    unite(col = "Pattern", 2:41, sep = " ")
-  temp_diff <- t(mapply('==', 
-                        strsplit(as.character(text), split = " "), 
-                        strsplit(as.character(cgfdb$Pattern), split = " ")))
-  differences <- as.data.frame(apply(temp_diff, 1, paste, collapse = " "))
-  names(differences) <- "logical"
-  remove(temp_diff)
-  cgfdb <- cgfdb %>%
-    mutate(logical = differences$logical,
-           num_F = str_count(differences$logical, "FALSE"),
-           perc_sim = (100 * (40 - num_F) / 40))
-  cgfdb <- cgfdb %>% 
-    group_by(cgf.type) %>%
-    mutate(num_isol = n())
-  rank_cgf_df <- cgfdb %>%
-    group_by(cgf.type) %>%
-    summarise(count = n()) %>%
-    mutate(rank = rank(count, ties.method = "min"))
-  cgfdb <- merge(cgfdb, rank_cgf_df, by.x = "cgf.type", by.y = "cgf.type")
-  
-  #TODO: change so user has option for output
-  # data <- cgfdb %>% select(-logical, -Pattern)
-  data <- cgfdb %>% select(strain.name, cgf.type, perc_sim, num_isol, rank,
-                           typing.lab, dataset.id_2, sample.origin, dataset.id_1,
-                           sample.type, source.general, source.specific_1, country,
-                           province, city, date.collected, campy.species, num_F)
-  data[data$perc_sim >= as.numeric(min_sim) & data$num_isol >= as.numeric(min_iso), ]
+filter_cgftype <- function(db, cgftype) {
+  temp <- db %>%
+    filter(cgftype %in% db.cgf_type)
 }
+
+get_order_stacked_bar_per_cgftype <- function(data) {
+  stats <- data %>%
+    group_by(cgf.type, source.specific_1) %>%
+    summarise(type_count = n()) %>%
+    mutate(prop = type_count/sum(type_count))
+  all_types <- as_data_frame(stats$cgf.type) %>% group_by(cgf.type=stats$cgf.type) %>% summarise()
+  order_types <- as_data_frame(x=list(cgf.type = stats$cgf.type, source.specific_1 = stats$source.specific_1, prop = stats$prop)) %>%
+    filter(source.specific_1 == "Chicken" | source.specific_1 == "Human")
+  merged_types <- merge(x=all_types, y=order_types, by.x="cgf.type", by.y = "cgf.type", all=TRUE)
+  max_type <- merged_types %>%
+    group_by(cgf.type) %>%
+    summarise(max = max(prop))
+  merged_type_order <- merge(x=merged_types, y=max_type, by.x=c("prop", "cgf.type"), by.y=c("max","cgf.type"))
+  order <- merged_type_order %>% arrange(source.specific_1, -prop)
+}
+
 
 #Stacked Barplot
 stacked_levels = c("Human", "Buffalo", "Cow", "Horse", "Raccoon", "Sheep", "Skunk", "Turkey", "Water", "Chicken")
 
 # server
 server <- function(input, output, session) {
-  re_min_sim <- reactive({input$min_perc_sim})
-  re_pattern <- reactive({input$pattern})
-  re_min_isol <- reactive({input$min_iso})
-  re_data_perc_sim <- reactive({perc_sim(re_pattern(), re_min_sim(), re_min_isol())})
-  re_data_by_cgf_type <- reactive({
-    re_data_perc_sim() %>%
-      group_by(cgf.type, num_isol, rank) %>%
-      summarise()
-    })
-  re_view <- reactive({
-    switch(input$view_type,
-           "strain.name" = re_data_perc_sim(),
-           "cgf.type" = re_data_by_cgf_type())
-    })
   
+  data_for_plots <- callModule(subsetData, "sidebar")
+
   #TAB1
-  output$table <- DT::renderDataTable(DT::datatable({
-    data <- re_view()
-    data
-  }))
+  re_view_type    <- reactive({input$view_type_in})
+  re_col          <- reactive({input$col_display})
+  re_agg_by       <- reactive({input$agg_by})
   
-  #TAB3
+  output$column_display_ui <- renderUI({
+    pickerInput(inputId = "col_display",
+                label = "Choose columns to display:",
+                options = list('actions-box' = TRUE),
+                choices = colnames(data_for_plots()),
+                selected = c("strain.name", "cgf.type", "typing.lab", "sample.origin", "source.specific_1"),
+                multiple = TRUE)
+  })
+
+  observeEvent(re_view_type(), {
+    if (re_view_type() == "aggregate") {
+      updatePickerInput(session,
+                      inputId = "col_display",
+                      selected = c("cgf.type"))
+    }
+    else {
+      updatePickerInput(session,
+                        inputId = "col_display",
+                        selected = c("strain.name", "cgf.type", "typing.lab", "sample.origin", "source.specific_1"))
+    }
+  })
+  
+  output$table <- DT::renderDataTable({
+    if (re_view_type() == "strain") {
+      data <- data_for_plots() %>% select(re_col())
+      DT::datatable(data, options = list(scrollX = TRUE))
+    }
+    else if (re_view_type() == "aggregate") {
+      cast_col <- paste(test, collapse = " + ")
+      data <- dcast(data_for_plots(), paste(cast_col, "~", "c(province, typing.lab)", sep=""), length)
+      data[(nrow(data) + 1), which(unlist(lapply(data, is.numeric)))] <- colSums(data[,which(unlist(lapply(data, is.numeric)))], na.rm=TRUE)
+      data[nrow(data), "cgf.type"] = "ALL"
+      DT::datatable(data, options = list(scrollX = TRUE))
+    }
+  })
+  
+  
+  #TAB4
   fillChosen <- reactive({input$fill_chosen})
   fillInput <- reactive({
     switch(fillChosen(),
-           "typing.lab" = re_data_perc_sim()$typing.lab,
-           "sample.origin" = re_data_perc_sim()$sample.origin,
-           "sample.type" = re_data_perc_sim()$sample.type,
-           "source.general" = re_data_perc_sim()$source.general,
-           "source.specific_1" = re_data_perc_sim()$source.specific_1,
-           "province" = re_data_perc_sim()$province
+           "typing.lab" = data_for_plots()$typing.lab,
+           "sample.origin" = data_for_plots()$sample.origin,
+           "sample.type" = data_for_plots()$sample.type,
+           "source.general" = data_for_plots()$source.general,
+           "source.specific_1" = data_for_plots()$source.specific_1,
+           "province" = data_for_plots()$province
            )
   })
   
   output$cgftypes_plot <- renderPlot({
-    bar_plot <- ggplot(re_data_perc_sim(), aes(x=cgf.type)) +
+    bar_plot <- ggplot(data_for_plots(), aes(x=cgf.type)) +
       geom_bar(aes(fill = fillInput())) +
       theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
       labs(x = "CGF Type", y = "Number of Isolates") + 
@@ -92,31 +97,22 @@ server <- function(input, output, session) {
     plot(bar_plot)
   })
   
-  #TAB4
+  #TAB5
   re_cgf_type <- reactive({input$cgf_type_in})
-  re_cgf_type_df <- reactive ({
-    re_data_perc_sim() %>% filter(cgf.type == as.character(re_cgf_type())) 
-  })
   re_cgf_count_df <- reactive({
-    tempdf <- re_cgf_type_df() %>% group_by(cgf.type) %>% summarise(count = n())
-    merge(tempdf, re_cgf_type_df(), by.x = "cgf.type", by.y = "cgf.type")
+    tempdf <- data_for_plots() %>% group_by(cgf.type) %>% summarise(count = n())
+    merge(tempdf, data_for_plots(), by.x = "cgf.type", by.y = "cgf.type")
   })
   
   re_x_axis <- reactive({input$x_axis})
   xaxisInput <- reactive({
     switch(re_x_axis(),
-           "typing.lab" = re_cgf_type_df()$typing.lab,
-           "sample.origin" = re_cgf_type_df()$sample.origin,
-           "sample.type" = re_cgf_type_df()$sample.type,
-           "source.general" = re_cgf_type_df()$source.general,
-           "source.specific_1" = re_cgf_type_df()$source.specific_1,
-           "province" = re_cgf_type_df()$province)
-  })
-  
-  observe({re_data_perc_sim()
-    updateSelectInput(session = session,
-                      inputId = "cgf_type_in",
-                      choices = re_data_perc_sim()$cgf.type)
+           "typing.lab" = data_for_plots()$typing.lab,
+           "sample.origin" = data_for_plots()$sample.origin,
+           "sample.type" = data_for_plots()$sample.type,
+           "source.general" = data_for_plots()$source.general,
+           "source.specific_1" = data_for_plots()$source.specific_1,
+           "province" = data_for_plots()$province)
   })
   
   output$cgftype_plot <- renderPlot({
@@ -127,77 +123,23 @@ server <- function(input, output, session) {
     plot(bar_plot)
   })
   
-  # TAB 5
-  get_order <- reactive({
-    stats <- re_data_perc_sim() %>%
-      group_by(cgf.type, source.specific_1) %>%
-      summarise(type_count = n()) %>%
-      mutate(prop = type_count/sum(type_count))
-    all_types <- as_data_frame(stats$cgf.type) %>% group_by(cgf.type=stats$cgf.type) %>% summarise()
-    order_types <- as_data_frame(x=list(cgf.type = stats$cgf.type, source.specific_1 = stats$source.specific_1, prop = stats$prop)) %>%
-      filter(source.specific_1 == "Chicken" | source.specific_1 == "Human")
-    merged_types <- merge(x=all_types, y=order_types, by.x="cgf.type", by.y = "cgf.type", all=TRUE)
-    max_type <- merged_types %>%
-      group_by(cgf.type) %>%
-      summarise(max = max(prop))
-    merged_type_order <- merge(x=merged_types, y=max_type, by.x=c("prop", "cgf.type"), by.y=c("max","cgf.type"))
-    order <- merged_type_order %>% arrange(source.specific_1, -prop)
+  # TAB 6: Prop of Sources
+  re_graph_type <- reactive({input$graph_type})
+  
+  observe({
+    if (re_graph_type() == 'Frequency of isolates shown using bar chart') {
+      callModule(barChartStackogram, "bar_graph_all", data_for_plots)
+    }
+    else {
+      print(data_for_plots())
+      callModule(propBarWidth, "wide_graph_all", data_for_plots)
+    }
   })
   
-  re_width <- reactive({
-    input$width
-  })
-  re_scale <- reactive({
-    switch(input$scale,
-           "linearly" = "identity",
-           "log10" = "log10",
-           "sqrt" = "sqrt"
-    )
-  })
-  re_prop_plot <- reactive({
-    ggplot(re_data_perc_sim() %>% select(cgf.type, source.specific_1), aes(x=cgf.type, y=1, width=re_width(),
-                      # fill= factor(source.specific_1, levels = stacked_levels),
-                      ordered=TRUE)) +
-      geom_bar(aes(fill = factor(source.specific_1, levels = stacked_levels)), 
-               position=position_fill(), stat="identity") +
-      scale_y_continuous(labels = scales::percent) +
-      labs(x="CGF Type", y = "Proportion", fill="Source") +
-      theme(axis.text.x = element_blank(),
-            axis.title.x = element_blank(),
-            axis.ticks.x = element_blank(),
-            plot.margin = unit(c(1,2,0,1), "cm"),
-            legend.position = "none") +
-      scale_fill_brewer(palette = "RdYlBu") +
-      scale_x_discrete(limits = get_order()$cgf.type)
-  })
-  re_count_plot <- reactive({
-    ggplot(re_data_perc_sim() %>% select(cgf.type, source.specific_1), aes(x=cgf.type, ordered = TRUE)) +
-      geom_bar(aes(fill = factor(source.specific_1, levels = stacked_levels)), width = re_width()) + 
-      labs(x="CGF Type", y = paste(re_scale(), "(Frequency)"), fill="Source") +
-      theme(axis.text.x = element_text(angle = 90, hjust = 1),
-            plot.margin = unit(c(0, 1, 1, 1), "cm"), legend.position = "bottom") +
-      scale_fill_brewer(palette = "RdYlBu") +
-      scale_x_discrete(limits = get_order()$cgf.type) +
-      scale_y_continuous(trans=re_scale())
-  })
-  output$stacked_prop_plot <- renderPlot({
-    align_plot <- ggarrange(re_prop_plot(), re_count_plot(), heights = c(2, 4), ncol = 1, nrow = 2, align = "v")
-    plot(align_plot)
-  })
+  #Tab 7 : Bubble source vs sample plot  
+  callModule(riskogram, "riskogram", data_for_plots)
   
-  #Tab 8
-  output$comp_graph_plot <- renderPlotly({
-    xaxisInput <- reactive({
-      switch(input$x_axis_comp,
-             "typing.lab" = re_data_perc_sim()$typing.lab,
-             "sample.origin" = re_data_perc_sim()$sample.origin,
-             "sample.type" = re_data_perc_sim()$sample.type,
-             "source.general" = re_data_perc_sim()$source.general,
-             "source.specific_1" = re_data_perc_sim()$source.specific_1,
-             "province" = re_data_perc_sim()$province)
-    })
-    comp_plot <- ggplot(re_data_perc_sim(), aes(x=xaxisInput(), fill=source.general)) + 
-      geom_bar()
-    plot(comp_plot)
-  })
+  #Tab 8 
+  callModule(attrEstStackogram, "attr_est", data_for_plots)
+
 }
--- ui.R
+++ ui.R
@@ -1,107 +1,88 @@
-library(shiny)
-
 
 text ="0 0 1 1 1 1 0 1 1 1 1 0 0 0 0 1 1 1 1 1 1 0 1 1 0 0 1 1 1 1 1 0 0 1 1 0 1 0 0 0"
 
+
 ui <- fluidPage(
-  tabsetPanel(
-    tabPanel("Query Tool Setup",
-             headerPanel("Fingerprint Query Tool"),
-             fluidRow(
-               column(3, textInput(inputId = "pattern",
-                                   label = "CGF Pattern to Search: ",
-                                   value = text)),
-               column(3, selectInput(inputId = "min_perc_sim",
-                                     label = "Minimum % Similarity:",
-                                     choices = seq(0, 100, 5),
-                                     selected = 85)),
-               column(3, textInput(inputId = "min_iso",
-                                   label = "Minimum # of Isolates:",
-                                   value = 10))
-             ),
-             fluidRow(
-               column(3, h3("Summary of Data:"),
-                      offset = 0.25)
-             ),
-             fluidRow(
-               column(3, selectInput(inputId = "view_type",
-                                     label = "View by: ",
-                                     choices = c("strain.name", "cgf.type")))
-             ),
-             fluidRow(
-               DT::dataTableOutput(outputId = "table")
-             )
-    ),
-    tabPanel("Pattern Match",
-             headerPanel("Pattern Match")
-    ),
-    tabPanel("Metadata",
-             headerPanel("Metadata")
-    ),
-    tabPanel("All CGF Types",
-             headerPanel("Bar Graph of all CGF Types"),
-             fluidRow(
-               column(3, selectInput(inputId = "fill_chosen",
-                                     label = "Fill Value:",
-                                     choices = c("typing.lab", "sample.origin", "sample.type", "source.general",
-                                                 "source.specific_1", "province"),
-                                     selected = "typing.lab"))
-             ),
-             fluidRow(
-               plotOutput(outputId = "cgftypes_plot")
-             )
-    ),
-    tabPanel("Individual CGF Types",
-             fluidRow(
-               headerPanel("Bar Graph of Individual CGF Type"),
-               column(3, selectInput(inputId = "cgf_type_in",
-                                     label = "CGF Type",
-                                     choices = NULL)),
-               column(3, selectInput(inputId = "x_axis",
-                                     label = "X-axis:",
-                                     choices = c("typing.lab", "sample.origin", "sample.type", "source.general",
-                                                 "source.specific_1", "province"),
-                                     selected = "typing.lab"))
-             ),
-             fluidRow(
-               plotOutput(outputId = "cgftype_plot")
-             )
-    ),
-    tabPanel("Proportion of Sources",
-             titlePanel("Proportion and Frequency of Sources vs CGF Type"),
-             fluidRow(
-               plotOutput("stacked_prop_plot")
-             ),
-             fluidRow(
-               column(4, 
-                      sliderInput(inputId = "width",
-                                  label = "Width of Bars:",
-                                  min = 0.8,
-                                  max = 1.0,
-                                  value = 0.968),
-                      offset = 1),
-               column(4,
-                      radioButtons(inputId = "scale",
-                                   label = "Frequency Graph Scale:",
-                                   choices = c("linearly", "log10", "sqrt")),
-                      offset = 2)
-             )
-    ),
-    tabPanel("Source vs Sample",
-             titlePanel("Percentage of Source vs Percentage of Sample for CGF Types")
-    ),
-    tabPanel("Composition Graphs",
-             titlePanel("Composition Graph"),
-             fluidRow(
-               column(3, selectInput(inputId = "x_axis_comp",
-                                     label = "X-axis:",
-                                     choices = c("typing.lab", "sample.origin", "sample.type", "source.general",
-                                                 "source.specific_1", "province"),
-                                     selected = "typing.lab"))
-             ),
-             fluidRow(
-               plotlyOutput("comp_graph_plot")
-             )
-    )
-  )
+   sidebarPanel(
+     headerPanel("Query Tool"),
+     subsetDataInput("sidebar")
+   ),
+   mainPanel(
+     tabsetPanel(
+       tabPanel("Data Table",
+                 mainPanel(
+                   fluidRow(
+                     DT::dataTableOutput(outputId = "table")
+                   ),
+                   fluidRow(
+                     column(3, selectInput(inputId = "view_type_in",
+                                           label = "View: ",
+                                           choices = c("By Strain" = "strain", "Aggregate" = "aggregate"))),
+                     column(8, uiOutput("column_display_ui")
+                     )
+                   ),
+                   fluidRow(
+                     uiOutput("agg_by")
+                   )
+                 )
+        ),
+        tabPanel("Pattern Match",
+                 headerPanel("Pattern Match")
+        ),
+        tabPanel("Metadata",
+                 headerPanel("Metadata")
+        ),
+        tabPanel("Individual CGF Type",
+                 headerPanel("Bar Graph of Individual CGF Type"),
+                 fluidRow(
+                   column(3, selectInput(inputId = "fill_chosen",
+                                         label = "Fill Value:",
+                                         choices = c("typing.lab", "sample.origin", "sample.type", "source.general",
+                                                     "source.specific_1", "province"),
+                                         selected = "typing.lab"))
+                 ),
+                 fluidRow(
+                   plotOutput(outputId = "cgftypes_plot")
+                 )
+        ),
+        tabPanel("Aggregate CGFTypes",
+                 fluidRow(
+                   headerPanel("Bar Graph of Aggregated CGFTypes"),
+                   column(3, selectInput(inputId = "x_axis",
+                                         label = "X-axis:",
+                                         choices = c("typing.lab", "sample.origin", "sample.type", "source.general",
+                                                     "source.specific_1", "province"),
+                                         selected = "typing.lab"))
+                 ),
+                 fluidRow(
+                   plotOutput(outputId = "cgftype_plot")
+                 )
+        ),
+        tabPanel("Proportion of Sources",
+                 titlePanel("Proportion and Frequency of Sources vs CGF Type"),
+                 fluidRow(
+                   selectInput(inputId = "graph_type",
+                               label = "Choose Type of Graph to Display:",
+                               choices = c("Frequency of isolates shown using bar chart",
+                                           "Frequency of isolates shown as width of bars"),
+                               selected = "")
+                 ),
+                 
+                 conditionalPanel(condition =  "input.graph_type == 'Frequency of isolates shown using bar chart'",
+                                  barChartStackogramUI("bar_graph_all")
+                 ),
+                 
+                 conditionalPanel(condition = "input.graph_type == 'Frequency of isolates shown as width of bars'",
+                                  propBarWidthUI("wide_graph_all")
+                 )
+                 
+        ),
+        tabPanel("Risk Assessment",
+                 riskogramUI("riskogram")
+         
+        ),
+       tabPanel("Stackogram with Attribute Estimation",
+                attrEstStackogramUI("attr_est"))
+     )
+   )
 )
